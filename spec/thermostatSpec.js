6b92fdec2bbb9c10fb14fefa6cbc8506e1647849
John Newman <newmanj903@gmail.com>
2018-05-15 15:58:11 +0100

    temp test passed

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,12 @@
+describe('thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('has a default temperature', function(){
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+});
